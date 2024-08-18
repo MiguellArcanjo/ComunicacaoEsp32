e09842341016cdc6e5374b29e45abda51e661ccc
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2024-08-17 22:32:03 -0300

    Finalização do Front

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,13 +1,29 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [on, setOn] = useState(true);
+
+  const clickButton = () => {
+    setOn(!on)
+
+    
+  }
+  
+  useEffect(() => {
+    console.log(on)
+  }, [on])
+
   return (
     <main className={styles.main}>
 
-      <span className={styles.containerBall}>
-        <span className={styles.ball1}></span>
-        <span className={styles.ball2}></span>
+      <h1 className={styles.title}>The door is closed?</h1>
+
+      <span className={on ? styles.containerBallRigth : styles.containerBallLeft}>
+        <span className={on ? styles.ballRight : styles.ballLeft } onClick={clickButton}></span>
       </span>
       
     </main>
