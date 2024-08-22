4d7bd1179949065c3bed4e0c6a20a8a94f07bdbf
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2024-08-22 19:43:53 -0300

    Implementação do websocket

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,39 +1,61 @@
 'use client';
 
 import styles from "./page.module.css";
-import axios from 'axios'
+import axios from 'axios';
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const apiUrl = "https://api--esp32.vercel.app/values"
-  const [dados, setDados] = useState()
+  const apiUrl = "http://localhost:2999/values";
+  const [dados, setDados] = useState();
   const [on, setOn] = useState();
-  
-  useEffect(() => {
 
+  useEffect(() => {
+    // Conectar ao WebSocket
+    const ws = new WebSocket('ws://localhost:2999');
+
+    ws.onopen = () => {
+      console.log('Conectado ao WebSocket');
+    };
+
+    ws.onmessage = (event) => {
+      const newValue = JSON.parse(event.data);
+      setDados(newValue);
+      setOn(newValue.comunicationValues);
+    };
+
+    ws.onclose = () => {
+      console.log('Desconectado do WebSocket');
+    };
+
+    ws.onerror = (error) => {
+      console.error('Erro no WebSocket:', error);
+    };
+
+    // Buscar o valor inicial
     axios.get(apiUrl)
       .then(response => {
-        const lastItem = response.data[response.data.length - 1]
+        const lastItem = response.data[response.data.length - 1];
         if (lastItem) {
           setDados(lastItem);
-          setOn(lastItem.comunicationValues)
+          setOn(lastItem.comunicationValues);
         }
       })
-      .catch((error) => console.error("Não foi possivel retornar o valor"))
-  }, [])
+      .catch((error) => console.error("Não foi possível retornar o valor"));
 
-  console.log(on)
+    // Limpar a conexão WebSocket quando o componente for desmontado
+    return () => {
+      ws.close();
+    };
+  }, []);
 
+  console.log(on);
 
   return (
     <main className={styles.main}>
-
       <h1 className={styles.title}>The door is closed?</h1>
-
       <span className={on ? styles.containerBallRight : styles.containerBallLeft}>
-        <span className={on ? styles.ballRight : styles.ballLeft }></span>
+        <span className={on ? styles.ballRight : styles.ballLeft}></span>
       </span>
-      
     </main>
   );
 }
